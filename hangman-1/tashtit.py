337ae0f38cf7e19c6f6b9fce75bcd0ce0fe74cec
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-04-13 11:42:33 +0300

    5

--- hangman-1/tashtit.py
+++
@@ -1,4 +0,0 @@
-#3.5.2
-txt=input("\nplease enter a word : ")                                  #קלט של מחרוזת
-count_num = len(txt)                                                 #אורך המחרוזת
-print("_ " * count_num)
--- hangman-1/the game.py
+++ hangman-1/the game.py
@@ -7,25 +7,25 @@
 #	print("sorry",x," isn't in the word")   #פלט- התו לא נמצא
 
 #התכנית מקבלת מילה ומדפיסה את אורכה ע"י קו תחתון כמספר האותיות
-#txt = input("enter a word :")
-#length =len(txt) 
-#print("the lengtf of the word you chose is :",length," letters")
-#print("_ "*length)
+txt = input("enter a word :")
+length =len(txt) 
+print("the lengtf of the word you chose is :",length," letters")
+print("_ "*length)
 
 
 	
-                              #התכנית מבקשת להקליד אות אחת ובודקת אם המשתמש אכן ביצע, ואם לא - מודיעה לו 
-txt=input("type only letters : ")           #הקלדת מילה
-length = len(txt)                           #השמת ערך אורך המילה לתא לנגט
-only_alpha = txt.isalpha()                  #בדיקה אם המחרוזת מכילה רק אותיות ומחזירה כן/לא לתא אונלי-אלפא
-if length > 1 and only_alpha==False:        #בדיקה אם המחרוזת ארוכה מאות אחת וגם לא אותיות         
-	print("E3")                             #פלט-אי3
-elif length > 1:                            #בדיקה אם אורך המילה גדול מ1
-	print("E1")                             # פלט - אי1
-elif  only_alpha ==False:                   #בדיקה אם בקלט יש סימן שאינו אות                                       
-	print("E2")                             # פלט - אי2                   
-else:                                       #אחרת
-	print(txt.lower())	                    #פלט - אות קטנה בכל מקרה
+#                               #התכנית מבקשת להקליד אות אחת ובודקת אם המשתמש אכן ביצע, ואם לא - מודיעה לו 
+# txt=input("type only letters : ")           #הקלדת מילה
+# length = len(txt)                           #השמת ערך אורך המילה לתא לנגט
+# only_alpha = txt.isalpha()                  #בדיקה אם המחרוזת מכילה רק אותיות ומחזירה כן/לא לתא אונלי-אלפא
+# if length > 1 and only_alpha==False:        #בדיקה אם המחרוזת ארוכה מאות אחת וגם לא אותיות         
+# 	print("E3")                             #פלט-אי3
+# elif length > 1:                            #בדיקה אם אורך המילה גדול מ1
+# 	print("E1")                             # פלט - אי1
+# elif  only_alpha ==False:                   #בדיקה אם בקלט יש סימן שאינו אות                                       
+# 	print("E2")                             # פלט - אי2                   
+# else:                                       #אחרת
+# 	print(txt.lower())	                    #פלט - אות קטנה בכל מקרה
 	
 	
 	
